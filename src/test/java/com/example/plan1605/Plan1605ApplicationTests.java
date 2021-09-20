58ca0dfd2ea1668769c99b9a40bd312f6453047d
Igor <igor.savchenko1158@gmail.com>
2021-09-20 23:20:08 +0300

    Add smoke test

---
+++ src/test/java/com/example/plan1605/Plan1605ApplicationTests.java
@@ -0,0 +1,28 @@
+package com.example.plan1605;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles({"h2db", "debug"})
+class Plan1605ApplicationTests {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate rest;
+
+    @Test
+    void contextLoads() {
+        assertNotNull(rest);
+        assertNotEquals(0, port);
+    }
+
+}
